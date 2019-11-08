6affd0801890584192592b28b4515b90fd89597b
WongY3306 <2642630866@qq.com>
2019-11-08 15:32:37 +0800

    组件

--- src/main.js
+++ src/main.js
@@ -7,11 +7,7 @@ import MintUI from "mint-ui";
 import "mint-ui/lib/style.css";
 import App from "./App.vue";
 import store from "./store";
-<<<<<<< HEAD
 import router from "./router";
-=======
-import router from "./router"
->>>>>>> ef47fec5a3c57221b1f02ea6fc7a8b323f14df8e
 Vue.use(VueAxios, axios);
 Vue.use(MintUI);
 Vue.use(ElementUI);
--- src/views/el-msite.vue
+++ src/views/el-msite.vue
@@ -1,20 +1,31 @@
 <template>
   <div id="msite">
     <el_Header></el_Header>
+    <div class="elw-sTitle">
+      <div>
+        <span class="el-icon-house"></span>
+        <span>附近商家</span>
+      </div>
+    </div>
     <el_hotList v-for="(i,$index) in list_arr" :key="$index">
       <div slot="el-conImg">
         <img :src="'http://elm.cangdu.org/img/'+i.image_path" alt />
       </div>
       <div slot="el-conName">{{i.name}}</div>
       <div slot="el-stater">
-        <el-rate v-model="i.rating" disabled show-score text-color="#ff9900" score-template="{value}" disabled-void-color="#666"></el-rate>
+        <el-rate
+          v-model="i.rating"
+          disabled
+          show-score
+          text-color="#ff9900"
+          score-template="{value}"
+          disabled-void-color="#666"
+        ></el-rate>
       </div>
-      <div slot="el-numBer">
-          月售106单
-      </div>
+      <div slot="el-numBer">月售106单</div>
       <div slot="el-conRots">¥20起送/{{i.piecewise_agent_fee.tips}}</div>
-       <div slot="el-conWz">{{i.distance}}/</div>
-       <div slot="el-conTime">{{i.order_lead_time}}</div>
+      <div slot="el-conWz">{{i.distance}}/</div>
+      <div slot="el-conTime">{{i.order_lead_time}}</div>
     </el_hotList>
   </div>
 </template>
@@ -24,27 +35,45 @@ import el_Header from "./../components/el-header/el-header";
 import el_hotList from "./../components/el-hotList/el-hotList";
 export default {
   components: { el_Header, el_hotList },
-  data(){
-      return{
-          value:3.4,
-          list_arr:'',
-      }
+  data() {
+    return {
+      value: 3.4,
+      list_arr: ""
+    };
   },
-  created(){
-      this.axios.get('http://elm.cangdu.org/shopping/restaurants?latitude=39.0475&longitude=117.42993&offset=0&limit=20&extras[]=activities&keyword=&restaurant_category_id=&restaurant_category_ids[]=&order_by=&delivery_mode[]=').then(res => {
-      this.list_arr=res.data
-      console.log(this.list_arr)
-    })
-    this.$emit('rating')
+  created() {
+    this.axios
+      .get(
+        "http://elm.cangdu.org/shopping/restaurants?latitude=39.0475&longitude=117.42993&offset=0&limit=20&extras[]=activities&keyword=&restaurant_category_id=&restaurant_category_ids[]=&order_by=&delivery_mode[]="
+      )
+      .then(res => {
+        this.list_arr = res.data;
+        console.log(this.list_arr);
+      });
+    this.$emit("rating");
   }
 };
 </script>
 
 <style>
-#msite{
-    padding-top:130px; 
+#msite {
+  padding-top: 130px;
 }
 .el-icon-arrow-left {
   font-size: 42px;
+}
+.elw-sTitle {
+  height: 96px;
+  line-height: 96px;
+  margin-bottom: 20px;
+  font-size:44px;
+  width: 100%;
+  color: #999;
+}
+.elw-sTitle span{
+    padding: 0 10px;
+}
+.elw-sTitle div{
+    padding: 20px;
 }
 </style>
